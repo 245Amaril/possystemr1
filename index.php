4f4cda682ce199dd8d362373bce52adfaa415d7f
245Amaril <24500036.amaril@tsu.ac.id>
2025-06-23 23:10:17 +0700

    Edit warna header editproduct

--- index.php
+++ index.php
@@ -303,8 +303,8 @@
     <div class="modal fade" id="editProductModal" tabindex="-1" aria-labelledby="editProductModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header bg-primary text-white">
-                    <h5 class="modal-title"></i> Edit Produk</h5>
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit Produk</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
